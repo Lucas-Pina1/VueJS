c2cef96e559e32a3ccd1937d5e2ceb837748adbe
Lucas Pina <ti.lucaspina@gmail.com>
2022-05-16 08:59:44 -0300

    todo final

--- tarefas/src/components/TaskGrid.vue
+++ tarefas/src/components/TaskGrid.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="task-grid">
     <template v-if="tasks.length > 0">
-      <Task v-for="task in tasks" :key="task.name" :task="task"></Task>
+      <Task v-for="(task, i) in tasks" :key="task.name" @taskDeleted="emit('taskDeleted', i)" @taskStateChanged="$emit('taskStateChanged', i)" :task="task"></Task>
     </template>
     <p v-else class="no-task">Sua vida está em dia :)</p>
   </div>
 </template>
 
 <script>
-import Task from "./Task.vue";
+import Task from './Task.vue';
 
 export default {
   components: { Task },
